2cf6b22d988e6bc8c51e5d770a6154e29f9bb9d5
rafaiy <rafaiy@Mac-Admin.local>
2018-02-27 19:16:55 +0300

    Initial Commit

---
+++ contacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  contacts
+//
+//  Created by rafaiy on 27/02/2018.
+//  Copyright © 2018 rafaiy.tabledview. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
